1560838093c488d22761b2201d27c1068f8ed81b
Micro_han <44750451+Micro-han@users.noreply.github.com>
2021-10-25 20:27:09 +0800

    Update large.py

--- dlgo/networks/large.py
+++ dlgo/networks/large.py
@@ -4,8 +4,10 @@ from keras.layers.convolutional import Conv2D, ZeroPadding2D
 
 
 # 围棋深度学习网络
+# 七层神经网络
 def layers(input_shape):
     return [
+        # 填0 64个输出过滤器 卷积核为7*7
         ZeroPadding2D((3, 3), input_shape=input_shape, data_format='channels_first'),
         Conv2D(64, (7, 7), padding='valid', data_format='channels_first'),
         Activation('relu'),
